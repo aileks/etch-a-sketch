2251206a642f3bfd0188f14c3e45dba6fc9112bc
aileks <88669313+aileks@users.noreply.github.com>
2023-10-06 08:49:27 -0400

    Basic skeleton

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-const container = document.getElementById("box-container");
+const container = document.getElementById("grid-container");
 
-for (let i = 0; i < 256; i++) {
-  let newDiv = document.createElement("div");
-  newDiv.className = "box";
-  container.appendChild(newDiv);
-};
+function createDivs(num = 16) {
+  container.style.setProperty("--grid-rows", num);
+  container.style.setProperty("--grid-cols", num);
+
+  for (let i = 0; i < (num * num); i++) {
+    let newDiv = document.createElement("div");
+    newDiv.id = i + 1;
+    newDiv.className = "grid";
+    newDiv.addEventListener("mouseover", () => newDiv.style.background = "black");
+    container.appendChild(newDiv);
+  };
+}
+
+function createUserGrid(gridSize) {
+  if (gridSize > 100){
+    alert("ERROR: Please choose a smaller number.")
+  } else {
+    clearGrid();
+    createDivs(gridSize);
+  };
+}
+
+function getUserInput() {
+  let userInput = prompt("Enter grid size:");
+  return userInput;
+}
+
+function clearGrid() {
+  container.querySelectorAll("*").forEach((e) => e.remove());
+  createDivs();
+}
+
+createDivs();
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", () => clearGrid());
+
+const changeSizeButton = document.getElementById("change-size");
+changeSizeButton.addEventListener("click", () => createUserGrid(getUserInput()));
