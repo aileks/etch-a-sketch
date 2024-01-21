fd37497f8c7cfb7560643cf3845349a9db8096ed
aileks <88669313+aileks@users.noreply.github.com>
2024-01-21 18:46:32 -0500

    Migrated to using a modal for user input

--- src/script.js
+++ src/script.js
@@ -1,5 +1,34 @@
 const container = document.getElementById("grid-container");
 
+
+function showModal(title, message) {
+  const modal = document.getElementById("modal");
+  const modalContent = document.getElementById("modal-content");
+  const modalTitle = document.getElementById("modal-title");
+  const modalMessage = document.getElementById("modal-text");
+  const input = document.getElementById("input");
+
+  if (modal.classList.contains("hide")) {
+    modal.classList.remove("hide");
+  }
+
+  modalTitle.textContent = title;
+  modalMessage.textContent = message;
+  input.value = "";
+  modal.style.display = "block";
+  input.focus();
+}
+
+function closeModal() {
+  const modal = document.getElementById("modal");
+  const modalContent = document.getElementById("modal-content");
+
+  modalContent.classList.add("hide");
+  modal.classList.add("hide");
+
+  modal.style.display = "none";
+}
+
 function createDivs(num = 32) {
   container.style.setProperty("--grid-rows", num);
   container.style.setProperty("--grid-cols", num);
@@ -30,14 +59,16 @@ function createUserGrid(gridSize) {
 }
 
 function getUserInput() {
-  let userInput = prompt("Enter grid size (Max: 100):");
+  const input = document.getElementById("input");
+  const userInput = input.value;
 
-  if (!userInput || isNaN(userInput) || !Number.isInteger(parseFloat(userInput)) || userInput < 8 || userInput > 100) {
-    alert("ERROR: Please enter whole number between 8 and 100.");
+  if (!userInput || isNaN(userInput) || !Number.isInteger(parseFloat(userInput))) {
+    showModal("ERROR", "Please enter whole number between 8 and 100.");
     return;
   }
 
-  return userInput;
+  createUserGrid(parseInt(userInput, 10));
+  closeModal();
 }
 
 function clearGrid() {
@@ -51,9 +82,7 @@ clearButton.addEventListener("click", () => clearGrid());
 
 const changeSizeButton = document.getElementById("change-size");
 changeSizeButton.addEventListener("click", () => {
-  let input = getUserInput();
-  if (!input) return;
-  createUserGrid(input);
+  showModal("Change Grid Size", "Enter your desired grid size (MAX: 100):")
 });
 
 document.addEventListener("touchmove", handleTouchMove, { passive: false });
